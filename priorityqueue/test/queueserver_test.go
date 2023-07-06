2214e58930178da42d77194b84ff28ea7ee4066b
AnasB <rafa0anas@gmail.com>
2023-07-07 00:06:14 +0200

    first commit queue + api client to local sdiff

--- priorityqueue/test/queueserver_test.go
+++ priorityqueue/test/queueserver_test.go
@@ -1,8 +1,8 @@
 package test
 
 import (
-	"ExecServerGo/go_proto/generated"
 	"context"
+	prot "generated_proto"
 	"main/queueimplem"
 	"main/queueserver"
 	"reflect"
@@ -24,74 +24,76 @@ func Test_main(t *testing.T) {
 	}
 }
 
-func Test_queueserver_AddJobToQueue(t *testing.T) {
-	type fields struct {
-		UnimplementedQueueOpsServer generated.UnimplementedQueueOpsServer
-	}
-	type args struct {
-		in0 context.Context
-		in1 *generated.AddJobRequest
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *generated.AddJobReply
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-		{
-			name: "test1",
-			fields: fields{
+//todo for later
 
-				UnimplementedQueueOpsServer: generated.UnimplementedQueueOpsServer{},
-			},
-			args: args{
-				context.Background(),
-				&generated.AddJobRequest{
-					Priority: 0,
-					Message:  "hello",
-				},
-			},
-			want: &generated.AddJobReply{
-				JobId: "lmao",
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				qu := queueserver.QueueServer{
-					UnimplementedQueueOpsServer: tt.fields.UnimplementedQueueOpsServer,
-				}
-				got, err := qu.AddJobToQueue(tt.args.in0, tt.args.in1)
-
-				if (err != nil) != tt.wantErr {
-					t.Errorf("AddJobToQueue() error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				if !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("AddJobToQueue() got = %v, want %v", got, tt.want)
-				}
-			},
-		)
-	}
-}
+//func Test_queueserver_AddJobToQueue(t *testing.T) {
+//	type fields struct {
+//		UnimplementedQueueOpsServer prot.UnimplementedQueueOpsServer
+//	}
+//	type args struct {
+//		in0 context.Context
+//		in1 *prot.AddJobRequest
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		want    *prot.AddJobReply
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//		{
+//			name: "test1",
+//			fields: fields{
+//
+//				UnimplementedQueueOpsServer: prot.UnimplementedQueueOpsServer{},
+//			},
+//			args: args{
+//				context.Background(),
+//				&prot.AddJobRequest{
+//					Priority: 0,
+//					Message:  "hello",
+//				},
+//			},
+//			want: &prot.AddJobReply{
+//				JobId: "lmao",
+//			},
+//			wantErr: false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(
+//			tt.name, func(t *testing.T) {
+//				qu := queueserver.QueueServer{
+//					UnimplementedQueueOpsServer: tt.fields.UnimplementedQueueOpsServer,
+//				}
+//				got, err := qu.AddJobToQueue(tt.args.in0, tt.args.in1)
+//
+//				if (err != nil) != tt.wantErr {
+//					t.Errorf("AddJobToQueue() error = %v, wantErr %v", err, tt.wantErr)
+//					return
+//				}
+//				if !reflect.DeepEqual(got, tt.want) {
+//					t.Errorf("AddJobToQueue() got = %v, want %v", got, tt.want)
+//				}
+//			},
+//		)
+//	}
+//}
 
 func Test_queueserver_GetJobsFromQueue(t *testing.T) {
 	type fields struct {
-		UnimplementedQueueOpsServer generated.UnimplementedQueueOpsServer
+		UnimplementedQueueOpsServer prot.UnimplementedQueueOpsServer
 	}
 	type args struct {
 		in0 context.Context
-		in1 *generated.GetNJobs
+		in1 *prot.GetNJobs
 	}
 	tests := []struct {
 		name    string
 		fields  fields
 		args    args
-		want    *generated.GetNJobsReply
+		want    *prot.GetNJobsReply
 		wantErr bool
 	}{
 		// TODO: Add test cases.
